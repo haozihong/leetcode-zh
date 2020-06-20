0791dfe02d5b4cdc0abe97c37f337b8243d29f39
haozihong <haozihong@hotmail.com>
2020-06-20 11:16:39 -0400

    60. Permutation Sequence (using math.factorial)

--- archive/lc_60. Permutation Sequence.py
+++ archive/lc_60. Permutation Sequence.py
@@ -1,13 +1,10 @@
-P = [1]
-for i in range(2, 10):
-    P.append(P[-1] * i)
-    
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         A = list("123456789"[:n])
         k -= 1
-        for i in range(n-2, -1, -1):
-            t, k = k // P[i], k % P[i]
+        for i in range(n-1, 0, -1):
+            p = math.factorial(i)
+            t, k = k // p, k % p
             A.append(A.pop(t))
         A.append(A.pop(0))
         return "".join(A)
