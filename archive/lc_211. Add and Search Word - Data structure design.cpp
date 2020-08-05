ec7d791f84780f2f1b700435289a7469cb4d0e45
haozihong <haozihong@hotmail.com>
2020-08-05 15:59:47 -0400

    add another version using pointer

--- archive/lc_211. Add and Search Word - Data structure design.cpp
+++ archive/lc_211. Add and Search Word - Data structure design.cpp
@@ -1,13 +1,13 @@
 // Trie, Design, 
 
+// 2020-08-05
 class WordDictionary {
 private:
-    vector<vector<int>> trie{vector<int>(27)};
+    vector<array<int, 27>> trie{1};
     
 public:
     /** Initialize your data structure here. */
-    WordDictionary() {
-    }
+    WordDictionary() { trie[0].fill(0); }
     
     /** Adds a word into the data structure. */
     void addWord(const string &word) {
@@ -16,7 +16,8 @@ public:
             c -= 97;
             if (trie[p][c] == 0) {
                 trie[p][c] = trie.size();
-                trie.emplace_back(27);
+                trie.emplace_back();
+                trie.back().fill(0);
             }
             p = trie[p][c];
         }
@@ -36,6 +37,54 @@ public:
             }
         }
         return trie[p].back();
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
+
+
+// 2019-12-07 (edit on 2020-08-05)
+class WordDictionary {
+private:
+    struct TrieNode {
+        bool isWord = false;
+        array<TrieNode *, 26> child;
+        TrieNode(): { child.fill(nullptr); }
+    } root;
+    
+public:
+    /** Initialize your data structure here. */
+    WordDictionary(): root() {}
+    
+    /** Adds a word into the data structure. */
+    void addWord(const string &word) {
+        TrieNode *p = &root;
+        for (auto c : word) {
+            c -= 97;
+            if (!p->child[c]) p->child[c] = new TrieNode();
+            p = p->child[c];
+        }
+        p->isWord = true;
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(const string &word, int i = 0, TrieNode *p = nullptr) {
+        if (!p) p = &root;
+        for (; i < word.length() && word[i] != '.'; ++i) {
+            char c = word[i] - 97;
+            if (!p->child[c]) return false;
+            p = p->child[c];
+        }
+        if (i == word.size()) return p->isWord;
+        // then word[i] is '.'
+        for (int k=0; k<26; ++k)
+            if (p->child[k] && search(word, i+1, p->child[k])) return true;
+        return false;
     }
 };
 
