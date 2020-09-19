f96cb1c3e8e1643eec02d7860c0463dd5e65674a
haozihong <haozihong@hotmail.com>
2020-09-19 19:48:39 -0400

    1109. Corporate Flight Bookings

---
+++ archive/lc_1109. Corporate Flight Bookings.java
@@ -0,0 +1,14 @@
+// Array, Math
+
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] count = new int[n];
+        for (int[] b : bookings) {
+            count[b[0]-1] += b[2];
+            if (b[1] < n) count[b[1]] -= b[2];
+        }
+        for (int i=1; i<n; ++i)
+            count[i] += count[i-1];
+        return count;
+    }
+}
