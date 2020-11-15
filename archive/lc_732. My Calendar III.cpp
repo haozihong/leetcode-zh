0a53cf66b39bda268f4e6664cb388bb4295f4d79
haozihong <haozihong@hotmail.com>
2020-11-14 20:39:31 -0500

    732. My Calendar III

---
+++ archive/lc_732. My Calendar III.cpp
@@ -0,0 +1,30 @@
+// Segment Tree, Ordered Map
+
+class MyCalendarThree {
+public:
+    int mx = 1;
+    map<int, int> I;  // intervals
+    
+    MyCalendarThree() {}
+    
+    int book(int st, int ed) {
+        auto p = I.lower_bound(st);
+        if (p->first != st) {
+            I[st] = p == begin(I) ? 0 : (--p)->second;
+            p = I.find(st);
+        }
+        auto q = I.lower_bound(ed);
+        if (q->first != ed) {
+            I[ed] = q == begin(I) ? 0 : (--q)->second;
+            q = I.find(ed);
+        }
+        while (p != q) mx = max(mx, ++(p++)->second);
+        return mx;
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(start,end);
+ */
